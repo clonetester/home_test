54800f90d09dd71ebcabbf7c1039d9c33fcbb79b
PHATDUONG-ITC-F\phat.duong <phat.duong@mservice.com.vn>
2023-04-22 16:35:53 +0700

    secret var

--- js/test.js
+++ js/test.js
@@ -8,20 +8,30 @@
   // Initialize Firebase
 
   // Lấy dữ liệu từ Firebase Realtime Database
-  database.ref('/TEXT1').once('value').then(function(snapshot) {
+  database.ref('/data').once('value').then(function(snapshot) {
     var myElement = document.getElementById("abc");
 
     var data = snapshot.val();
-  
+    myElement.textContent = data.TEXT1;
+    database.goOffline();
     // Đặt nội dung vào phần tử HTML
-    myElement.textContent = data;
   });
-  database.ref('/CarouselHead1').once('value').then(function(snapshot) {
-    var myElement = document.getElementById("CarouselHead1");
 
-    var data = snapshot.val();
+const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+const chatId = '1811845009';
+document.getElementById('submitTele').addEventListener('click', () => {
+  const data = {
+    message: 'This is a message sent from my website'
+  };
   
-    // Đặt nội dung vào phần tử HTML
-    myElement.textContent = data;
+  axios.post(`https://api.telegram.org/bot${telegramBotToken}/sendMessage`, {
+    chat_id: chatId,
+    text: data.message
+  })
+  .then((response) => {
+    console.log('Message sent successfully:', response.data);
+  })
+  .catch((error) => {
+    console.error('Error sending message:', error);
   });
-  database.goOffline();
+});
